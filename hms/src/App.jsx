48b614913e9dce3f65736ba1b3d05974a33dd621
Ebrahim525 <m.ebrahim.n123@gmail.com>
2025-03-26 23:06:26 +0530

    commented Routing

--- hms/src/App.jsx
+++ hms/src/App.jsx
@@ -22,3 +22,37 @@
   }
 
   export default App
+
+  // import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import './App.css';
+// import AdminDashboard from './pages/AdminDashboard';
+// import UserDashboard from './pages/UserDashboard';
+// import HotelManagerDashboard from './pages/HotelManager';
+// import Login from './pages/Login';
+
+// function Navbar() {
+//   return (
+//     <nav>
+//       <Link to="/">Login</Link> | 
+//       <Link to="/admin">Admin Dashboard</Link> | 
+//       <Link to="/user">User Dashboard</Link> | 
+//       <Link to="/manager">Hotel Manager</Link>
+//     </nav>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/admin" element={<AdminDashboard />} />
+//         <Route path="/user" element={<UserDashboard />} />
+//         <Route path="/manager" element={<HotelManagerDashboard />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
