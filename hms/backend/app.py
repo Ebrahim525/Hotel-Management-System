21b94961966a1f03e4f87eef5b40d218fc6315e0
tanvi876 <138428583+tanvi876@users.noreply.github.com>
2025-03-29 17:10:22 +0530

    Update app.py

--- hms/backend/app.py
+++ hms/backend/app.py
@@ -116,7 +116,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
 
-from models.models import db, User  # Import only models first
+from models.models import db, User  
 
 app = Flask(__name__)
 
